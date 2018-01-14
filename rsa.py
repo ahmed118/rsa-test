34834fb4592c6afc8adc348b30b8303ed7c8f9c9
Ahmed Raza Memon <31822328+ahmed118@users.noreply.github.com>
2018-01-14 14:08:44 +0530

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -16,7 +16,7 @@ def egcd(a, b):
 def modinv(a, m):
      g, x, y = egcd(a, m)
      if g != 1:
-         raise Exception('modular inverse does not exist')
+         raise Exception('Inverse Modular doesn\'t exist..')
      else:
          return x % m
 
